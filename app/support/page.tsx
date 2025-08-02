79926a1d71e419b4d4025ea192ca8a6f4b64ec9d
Harjuna Tri Nugroho <harjunatn@Harjunas-MacBook-Pro.local>
2025-08-02 19:32:24 +0700

    adding default page

---
+++ app/support/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import type React from "react"
+
+export default function Support() {
+  
+
+  return (
+    <div className="pt-16">
+     {/* Hero Section */}
+      <section className="py-44 bg-gradient-to-br from-slate-50 to-sky-50">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center mb-16">
+            <h1 className="text-4xl sm:text-5xl font-bold text-slate-900 mb-6">Cooming Soon!</h1>
+            <p className="text-xl text-slate-600 max-w-3xl mx-auto">
+              bare with us as we prepare to launch our contact page. We are working hard to bring you a seamless experience for all your inquiries and support needs.
+            </p>
+          </div>
+          <div className="max-w-2xl mx-auto">
+            <p className="text-lg text-slate-500">
+              In the meantime, if you have any urgent questions or need assistance, please reach out to us via email at nayarta.com
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
